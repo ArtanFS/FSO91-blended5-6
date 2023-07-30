f8421c3e11bd1eb441adefcbde5b5753b0f05103
ArtanFS <art_anatoliy@ukr.net>
2023-07-30 15:09:47 +0300

    first 2 todo

---
+++ lesson-1/app.js
@@ -0,0 +1,25 @@
+//TODO: 1 ===================================
+// Якщо змінна a дорівнює 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
+// const a = 11;
+// if (a === 10) {
+//    return console.log("True");
+// }
+// console.log("False");
+// a === 10 ? console.log("True") : console.log("False");
+
+//TODO: 2 ===================================
+// У змінній min лежить число від 0 до 59. Визначте у котру чверть години
+// Попадає це число (у першу, другу, третю або четверту).
+
+// const min = 61;
+// if (min >= 0 && min <= 15) {
+//     console.log("First");
+// } else if (min > 15 && min <= 30) {
+//     console.log("Second");
+// } else if (min > 30 && min <= 45) {
+//     console.log("Third");
+// } else if (min > 45 && min <= 59) {
+//     console.log("Fourth");
+// } else {
+//     console.log('Wrong number');
+// }
